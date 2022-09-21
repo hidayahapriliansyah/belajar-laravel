778ad3297c4d13d7789f37c29a1ae7e53ee03df7
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2022-09-21 13:30:59 +0700

    authorization

--- application/coba-laravel/resources/views/dashboard/layouts/sidebar.blade.php
+++ application/coba-laravel/resources/views/dashboard/layouts/sidebar.blade.php
@@ -14,5 +14,18 @@
         </a>
       </li>
     </ul>
+    @can('admin')
+    <h6 class="sidebar-heading d-flex justify-content-between align-items-center mt-4 mb-1 px-3 text-muted">
+      <span>Administrator</span>
+    </h6>
+    <ul class="nav flex-column">
+      <li class="nav-item">
+        <a class="nav-link {{ Request::is('dashboard/categories*') ? 'active' : '' }}" aria-current="page" href="/dashboard/categories">
+          <span data-feather="grid"></span>
+          Post Categories
+        </a>
+      </li>
+    </ul>
+    @endcan
   </div>
 </nav>
